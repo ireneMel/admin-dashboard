cd65decaf27ec2b2a16bceec9b687fceb324b5a2
Iryna Melnyk <iramelnykv@gmail.com>
2023-03-27 16:06:52 -0400

    implement light and dark themes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+
 function App() {
+    cpmst[theme, colorMode] = useMode();
+
     return (
-        <div className="app">
-        </div>
+        <ColorModeContext.Provider value={colorMode}>
+            {/* add this so mui has acces to it */}
+            <ThemeProvider theme={theme}>
+                {/* resets css to default */}
+                <CssBaseline />
+                <div className="app">
+                    <main className="content"></main>
+                </div>
+            </ThemeProvider>
+        </ColorModeContext.Provider>
     );
 }
 
